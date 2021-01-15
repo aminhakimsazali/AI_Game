e40c734f0f88848610af93665085055703d5c051
406X <fedelic406@gmail.com>
2021-01-15 19:48:27 +0800

    Added condition for a draw game when each player only has one king left

--- Group Assignment/Python-Checkers-AI/TestAI.py
+++ Group Assignment/Python-Checkers-AI/TestAI.py
@@ -3,7 +3,7 @@ from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
 from checkers.game import Game
 from AI import AI
 from AI_2 import AI as AI2
-
+import time
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -30,41 +30,51 @@ def main():
 
     p1_wins = 0
     p2_wins = 0
-    rounds = 20
+    draw_count = 0
+    rounds = 10
     curr_round = 0
 
+    agent1_name ="Minimax with A-B Pruning"
+    agent2_name ="Monte Carlo Tree Search"
     while run:
         clock.tick(FPS)
         if game.turn == start_side[0]:
-            value, new_board = AIbot.minimax(game.get_board(), 3, float('-inf'), float('inf'), True,  start_side[0], game)
+            value, new_board = AIbot.minimax(game.get_board(), 4, float('-inf'), float('inf'), True,  start_side[0], game)
             if (new_board is not None):
                 game.ai_move(new_board)
             else:
                 no_moves=True
 
         elif game.turn == start_side[1]:
-            value, new_board = AIbot2.minimax(game.get_board(), 3, float('-inf'), float('inf'), True, start_side[1], game)
+            new_board = AIbot2.play(game.get_board(), start_side[1], game)
             if (new_board is not None):
                 game.ai_move(new_board)
             else:
                 no_moves=True
+        board = game.get_board()
+        if(board.red_left == 1 and board.white_left == 1 and board.white_kings == 1 and board.red_kings == 1):
+            print("Draw Game")
+            draw_count+=1
+            swapSide(start_side)
+            game.reset()
+
 
         if game.winner() != None:
             if(game.winner() == RED):
                 if(start_side[0]==RED):
                     p1_wins+=1
-                    print("RED Wins! (Agent 1)")
+                    print("RED Wins! (Agent 1 - " + agent1_name + " )")
                 else:
                     p2_wins+=1
-                    print("RED Wins! (Agent 2)")
+                    print("RED Wins! (Agent 2 - " + agent2_name + " )")
 
             else:
                 if(start_side[0]==WHITE):
                     p1_wins+=1
-                    print("WHITE Wins! (Agent 1)")
+                    print("WHITE Wins! (Agent 1 - " + agent1_name + " )")
                 else:
                     p2_wins+=1
-                    print("WHITE Wins! (Agent 1)")
+                    print("WHITE Wins! (Agent 2 - " + agent2_name + " )")
             curr_round += 1
             if (curr_round >= rounds):
                 run = False
@@ -77,17 +87,17 @@ def main():
             if game.turn == RED:
                 if(start_side[0]==RED):
                     p2_wins+=1
-                    print("WHITE Wins! (Agent 2)")
+                    print("WHITE Wins! (Agent 2 - " + agent2_name + " )")
                 else:
                     p1_wins+=1
-                    print("WHITE Wins! (Agent 1)")
+                    print("WHITE Wins! (Agent 1 - " + agent1_name + " )")
             else:
                 if(start_side[0]==WHITE):
                     p2_wins += 1
-                    print("RED Wins! (Agent 2)")
+                    print("RED Wins! (Agent 2 - " + agent2_name + " )")
                 else:
                     p1_wins += 1
-                    print("RED Wins! (Agent 1)")
+                    print("RED Wins! (Agent 1 - " + agent1_name + " )")
             curr_round += 1
             if (curr_round >= rounds):
                 run = False
@@ -111,8 +121,9 @@ def main():
         game.update()
 
 
-    print("Player 1 Win count: " + str(p1_wins))
-    print("Player 2 Win count: " + str(p2_wins))
+    print("Agent 1 Win count: " + str(p1_wins))
+    print("Agent 2 Win count: " + str(p2_wins))
+    print("Draw count: " + str(draw_count))
     pygame.quit()
 
 def swapSide(sides):
