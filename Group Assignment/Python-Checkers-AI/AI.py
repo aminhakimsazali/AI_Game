f13f7f206e12ad76aaa703e8eed9f72eb230024f
406X <fedelic406@gmail.com>
2021-01-15 20:53:45 +0800

    Adjusted heuristics

--- Group Assignment/Python-Checkers-AI/AI.py
+++ Group Assignment/Python-Checkers-AI/AI.py
@@ -1,48 +1,70 @@
 from copy import deepcopy
 import pygame
 
-RED = (255,0,0)
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
+
 class AI:
-    def __init__ (self):
+    def __init__(self):
         self.totalnodes = 0
 
     def explored_node(self):
         return self.totalnodes
 
     def minimax(self, position, depth, alpha, beta, max_player, turn, game):
-        self.totalnodes+=1
+        self.totalnodes += 1
         if depth == 0 or position.winner() != None:
-            return position.evaluate(), position
-        
+            return self.evalState(position,turn), position
+
         if max_player:
             maxEval = float('-inf')
             best_move = None
             for move in self.get_all_moves(position, turn, game):
-                evaluation = self.minimax(move, depth-1, alpha, beta, False, turn, game)[0]
+                evaluation = self.minimax(move, depth - 1, alpha, beta, False, turn, game)[0]
                 maxEval = max(maxEval, evaluation)
                 alpha = max(alpha, evaluation)
                 if beta <= alpha:
                     break
                 if maxEval == evaluation:
                     best_move = move
-            
+
             return maxEval, best_move
         else:
             minEval = float('inf')
             best_move = None
             for move in self.get_all_moves(position, turn, game):
-                evaluation = self.minimax(move, depth-1, alpha, beta, True, turn, game)[0]
+                evaluation = self.minimax(move, depth - 1, alpha, beta, True, turn, game)[0]
                 minEval = min(minEval, evaluation)
                 beta = min(beta, evaluation)
                 if beta <= alpha:
                     break
                 if minEval == evaluation:
                     best_move = move
-            
+
             return minEval, best_move
 
+    def evalState(self, move, turn):
+        score = 0
+        my_piece_count = 0
+        my_king_count = 0
+        enemy_king_count = 0
+        enemy_piece_count = 0
+
+        board = move
+
+        if (turn == RED):
+            my_king_count = board.red_kings
+            enemy_king_count = board.white_kings
+            my_piece_count = board.red_left
+            enemy_piece_count = board.white_left
+        else:
+            my_king_count = board.white_kings
+            enemy_king_count = board.red_kings
+            my_piece_count = board.white_left
+            enemy_piece_count = board.red_left
+        score = my_piece_count - (enemy_piece_count * 2) + (my_king_count * 3) - (enemy_king_count * 4)
+        return score
 
     def simulate_move(self, piece, move, board, game, skip):
         board.move(piece, move[0], move[1])
@@ -50,7 +72,6 @@ class AI:
             board.remove(skip)
 
         return board
-
 
     def get_all_moves(self, board, color, game):
         moves = []
@@ -63,16 +84,14 @@ class AI:
                 temp_piece = temp_board.get_piece(piece.row, piece.col)
                 new_board = self.simulate_move(temp_piece, move, temp_board, game, skip)
                 moves.append(new_board)
-        
-        return moves
 
+        return moves
 
     def draw_moves(self, game, board, piece):
         valid_moves = board.get_valid_moves(piece)
         board.draw(game.win)
-        pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
+        pygame.draw.circle(game.win, (0, 255, 0), (piece.x, piece.y), 50, 5)
         game.draw_valid_moves(valid_moves.keys())
         pygame.display.update()
-        #pygame.time.delay(100)
+        # pygame.time.delay(100)
 
-  
